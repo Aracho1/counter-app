eacbd580ed17166dbf801e258a2e97dbc28d7ef4
Ara <hello.ara@pm.me>
2020-12-15 10:45:39 +0000

    add decrement method, update last modified time

--- lib/counter.rb
+++ lib/counter.rb
@@ -1,4 +1,5 @@
 require_relative './database_connection'
+require 'date'
 
 class Counter
   def count
@@ -9,9 +10,25 @@ class Counter
   def increment
     read_count = count
     result = DatabaseConnection.query("UPDATE counter SET count = '#{read_count + 1}' WHERE id=1;")
+    update_time
+  end
+
+  def decrement
+    read_count = count
+    result = DatabaseConnection.query("UPDATE counter SET count = '#{read_count - 1}' WHERE id=1;")
+    update_time
   end
 
   def self.instance
     @counter ||= Counter.new
+  end
+
+  def time
+    result = DatabaseConnection.query("SELECT * FROM counter WHERE id=1;")
+    result[0]['time']
+  end
+
+  def update_time
+    DatabaseConnection.query("UPDATE counter SET time = '#{DateTime.now.strftime("%H:%M")}' WHERE id=1;")
   end
 end
